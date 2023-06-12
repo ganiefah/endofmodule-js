01c5a8c90cadb833ddbe8fe07019591314ffd675
ganiefah <ganiefahmajiet3@gmail.com>
2023-06-12 14:54:38 +0200

    products done

--- script/products.js
+++ script/products.js
@@ -1,37 +1,49 @@
 let catalog= [{
     id:1,
-    title:'Bottega Fragolino Rosso 750ml',
-    image: 'https://i.postimg.cc/cH0kjJ2j/Screenshot-2023-06-09-153701.png',
-    price: 'R 209,00' ,
-    modal: 'Limited Edition Giftbox.This sparkling wine is reowned for its unmistakable flavour of freshly-picked wild strawberries.The Fragolino Rosso is Sweet, fresh, lively, with intense hints of wild strawberries and fruit. Suitable at any time of the day, it is also an excellent aperitif.',
+    title:'Paricha',
+    image: 'https://i.postimg.cc/9Qp0J7yQ/19248-Parchira-1.jpg',
+    price: 'R 650,00' ,
+    modal: 'A “money tree” is actually multiple Pachira aquatica trees braided together during growth. Pachira aquatica is a broadleaf evergreen native to Central and South America and has many common names including Good Luck Tree, Malabar chestnut, Guiana Chestnut, French Peanut, Saba Nut, Monguba, Pumpo, Provision Tree, and Wild Kapok Tree.',
+},
+{
+    id:2,
+    title:'Dracaena',
+    image: 'https://i.postimg.cc/fTkBSCzj/17634-Dracaena.jpg',
+    price: 'R 770,00' ,
+    modal: ' Dracaena is one of the indoor plants that help reduce indoor pollution levels. Increase humidity: The plant releases water vapour and increases moisture levels in air, thus, reducing dry-air conditions. Ornamental purpose: These indoor plants help beautify home interiors.',
+},
+{
+    id:3,
+    title:'Caladium',
+    image: 'https://i.postimg.cc/hjZgMsHb/18147-Caladium.jpg',
+    price: 'R 390,00' ,
+    modal: ' Caladiums are tropical plants known for their big, heart-shaped leaves that display amazing color combinations of white, pink, red and green. Though caladiums rarely flower, their foliage guarantees a colorful show wherever they are planted -- beneath trees, between shrubs, in perennial borders or in containers.',
+},
+{
+    id:4,
+    title:'Monstera',
+    image: 'https://i.postimg.cc/mgh8vwz6/17753-Monstera.jpg',
+    price: 'R 180,00' ,
+    modal: 'Monstera deliciosa is a favourite plant among decorators thanks to its dramatic and unusual leaves, and it is also an ideal plant to attract money and good fortune according to Feng Shui.',
 },
 ]
-
-// <div class="card" style="width: 18rem;">
-//   <div class="card-body">
-//     <h5 class="card-title">Card title</h5>
-//     <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
-//     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-//     <a href="#" class="card-link">Card link</a>
-//     <a href="#" class="card-link">Another link</a>
-//   </div>
-// </div>
-let divCatalog = document.querySelector('.catalog');
-catalog.forEach((data)=> {
+let divCatalog = document.querySelector('#products');
+catalog.forEach((data, i)=> {
     divCatalog.innerHTML += 
-    `<div class="col column">
+    `
     <div class="card" style="width: 18rem;">
     <div class="card-body">
     <p class="text-black card-title">
     ${data.title}
     </p>
+
     <img src= "${data.image}" class="img-fluid">
     </p>
     <p class="text-black">${data.price}</p>
-    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+    <button type="button"  class="btn btn-outline-success " data-bs-toggle="modal" data-bs-target="#${data.id}">
     Read More
     </button>
-    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal fade" id="${data.id}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
@@ -39,7 +51,7 @@ catalog.forEach((data)=> {
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-        ${data.modal}
+        <p>${data.modal}</p>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
@@ -50,5 +62,138 @@ catalog.forEach((data)=> {
     <a href="/html/checkout.html" role="button" class="btn">Add To Cart</a>
     </div>
     </div>
-    </div> `
+   `
+   
+})
+
+let cat= [{
+  id:5,
+  title:'Oak Bonsai Tree',
+  image: 'https://i.postimg.cc/0Qnrv7hb/20992-Oak-Bonsai-Tree.jpg',
+  price: 'R 490,00' ,
+  modal: "Oak bonsai trees have a tremendous and stately appearance—deep, dark green lobes with show-stopping form. Potential hollows in the trunk and moderately coarse branching systems are the features that make the white oak like no other."
+},
+{
+  id:6,
+  title:'Fern Tree',
+  image: 'https://i.postimg.cc/j2RBBNQM/14881-Fern-Tree.jpg',
+  price: 'R 500,00' ,
+  modal: ' Apart from enhancing the aesthetics, they are also useful in removing harmful air pollutants from the environment. The  Ferns not only spruce up the air quality but also improve the humidity levels by restoring moisture to the air naturally, which gives a soothing effect.',
+},
+{
+  id:7,
+  title:'Olive Tree',
+  image: 'https://i.postimg.cc/63N95MNc/19849-Olive-Tree.jpg',
+  price: 'R 350,00' ,
+  modal: 'Olive leaf extract comes from the leaves of an olive plant. It contains an active ingredient called oleuropeinTrusted Source. This nutrient is thought to contribute to the anti-inflammatory and antioxidant properties of olive leaf extract.',
+},
+{
+  id:8,
+  title:'Bay Tree',
+  image: 'https://i.postimg.cc/446rgJdr/19857-Bay-Tree.jpg',
+  price: 'R 950,00' ,
+  modal: ' The bay tree is used as a valuable flavoring agent in the culinary and food industry. This plant is used in folk medicine, in stomachic and carminative remedies, and for the treatment of digestive disease.',
+},
+]
+let divCat = document.querySelector('#prod');
+cat.forEach((data, i)=> {
+  divCat.innerHTML += 
+  `
+  <div class="card" style="width: 18rem;">
+  <div class="card-body">
+  <p class="text-black card-title">
+  ${data.title}
+  </p>
+  <img src= "${data.image}" class="img-fluid">
+  </p>
+  <p class="text-black">${data.price}</p>
+  <button type="button"  class="btn btn-outline-success " data-bs-toggle="modal" data-bs-target="#${data.id}">
+  Read More
+  </button>
+  <div class="modal fade" id="${data.id}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal-dialog">
+  <div class="modal-content">
+    <div class="modal-header">
+      <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+    </div>
+    <div class="modal-body">
+      <p>${data.modal}</p>
+    </div>
+    <div class="modal-footer">
+      <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+    </div>
+  </div>
+</div>
+</div>
+  <a href="/html/checkout.html" role="button" class="btn">Add To Cart</a>
+  </div>
+  </div>
+ `
+})
+
+let cata= [{
+  id:9,
+  title:'Snowball Tree',
+  image: 'https://i.postimg.cc/bNMW4yjc/19819-Snowball-Tree-B.jpg',
+  price: 'R 1050,00' ,
+  modal: "Huge heads of white, hydrangea-like flowers are the main feature of this spectacular, spring-flowering shrub that enjoys a cool position. Since the flowers are the main attraction, its foliage is not as important as with other viburnums, but it does gain colour in autumn.  ."
+},
+{
+  id:10,
+  title:'Zebrina Plant',
+  image: 'https://i.postimg.cc/QtW7K8VD/Screenshot-2023-06-12-141357.png',
+  price: 'R 425,00' ,
+  modal: ' Apart from enhancing the aesthetics, they are also useful in removing harmful air pollutants from the environment. The  Ferns not only spruce up the air quality but also improve the humidity levels by restoring moisture to the air naturally, which gives a soothing effect.',
+},
+{
+  id:11,
+  title:'String Of Pearls Plant',
+  image: 'https://i.postimg.cc/yNZ5xB14/Screenshot-2023-06-12-141105.png',
+  price: 'R 400,00' ,
+  modal: 'Our string of pearls is an eye-catching trailing succulent that simply takes the outdoors in, providing a touch of greenery to any room and is ideal for placing on shelves and sideboards. It comes in a sturdy and stylish cement pot that accentuates and complements a range of home décor styles for maximum versatility.',
+},
+{
+  id:12,
+  title:'Onion Grass ',
+  image: 'https://i.postimg.cc/4xsNzJ2G/Screenshot-2023-06-12-142111.png',
+  price: 'R 430,00' ,
+  modal: 'Take this magnificent onion grass with a touch of the wild to bring in the outdoors; it is the ideal decorative element to liven up your living space instantly. It comes in a sturdy and stylish cement pot that accentuates and complements a range of home décor styles for maximum versatility.',
+},
+]
+let divCata = document.querySelector('#product');
+cata.forEach((data, i)=> {
+  divCata.innerHTML += 
+  `
+  <div class="card" style="width: 18rem;">
+  <div class="card-body">
+  <p class="text-black card-title">
+  ${data.title}
+  </p>
+  <img src= "${data.image}" class="img-fluid">
+  </p>
+  <p class="text-black">${data.price}</p>
+  <button type="button"  class="btn btn-outline-success " data-bs-toggle="modal" data-bs-target="#${data.id}">
+  Read More
+  </button>
+  <div class="modal fade" id="${data.id}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal-dialog">
+  <div class="modal-content">
+    <div class="modal-header">
+      <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+    </div>
+    <div class="modal-body">
+      <p>${data.modal}</p>
+    </div>
+    <div class="modal-footer">
+      <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+    </div>
+  </div>
+</div>
+</div>
+  <a href="/html/checkout.html" role="button" class="btn">Add To Cart</a>
+  </div>
+  </div>
+ `
 })
